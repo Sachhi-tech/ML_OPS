d150701cb663175d8223729c62bba670bcddc550
Sachhi-tech <sachhidanandabehuria66@gmail.com>
2026-09-05 02:47:20 +0530

    add ML model test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,2 +1,11 @@
-def test_basic_math():
-    assert 2 + 2 == 4
+from joblib import load
+
+
+def test_model_prediction():
+
+    model = load("Model/model.joblib")
+
+    prediction = model.predict([[8.0]])
+
+    assert prediction is not None
+    assert len(prediction) == 1
