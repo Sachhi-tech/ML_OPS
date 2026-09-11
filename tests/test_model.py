0b851429b25664a713153f48326743f614a996d5
Sachhi-tech <sachhidanandabehuria66@gmail.com>
2026-09-12 00:34:15 +0530

    changes in test_predict.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,17 +1,16 @@
-from pathlib import Path
-from joblib import load
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-MODEL_PATH = BASE_DIR / "Model" / "model.joblib"
+from sklearn.linear_model import LinearRegression
 
 
 def test_model_prediction():
 
-    model = load(MODEL_PATH)
+    model = LinearRegression()
 
-    prediction = model.predict([[8.0]])
+    model.fit(
+        [[1], [2], [3]],
+        [2, 4, 6]
+    )
+
+    prediction = model.predict([[4]])
 
     assert prediction is not None
     assert len(prediction) == 1
